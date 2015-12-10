53cddef8bdc776340149c8ece3cbea7dafd4c8c2
bingrong <bpu@SEA4-043.booking.pcln.com>
2015-12-10 13:50:17 -0800

    show hotel list logic

---
+++ app/scripts/app.js
@@ -0,0 +1,41 @@
+$(function(){
+    var list = [{
+        name:'dubai',
+        view:['city','mountain','natural'],
+        price: 2,
+        travletype:['family','single']
+    },{
+        name:'beijing',
+        view:['city','history','museum','mountain','shopping'],
+        price: 3,
+        travletype:['family','romantic','single']
+    }];
+    showResult(list);
+});
+
+function showResult(list){
+    $('#hotel-list ul').empty();
+    $.each(list, function(i, hotel){
+        var hoteldiv = $('#hotel').clone();
+        hoteldiv.click(function(){
+            location.href='result.html?city=' + hotel.name;
+        });
+        $(hoteldiv.find('.city-photo img')).attr('src','styles/image/city/'+hotel.name.toLowerCase()+'.jpg');
+        $(hoteldiv.find('.city-name')).html(hotel.name);
+        var price = $(hoteldiv.find('.city-price'));
+        for(var i=0;i<hotel.price;i++){
+            price.append("$");
+        }
+        var view = $(hoteldiv.find('.city-view'));
+        view.empty();
+        $.each(hotel.view, function(i,v){
+            view.append('<img src="styles/image/view/'+ v.toLowerCase()+'.png" title="'+v+'"/>');
+        });
+        var travletype = $(hoteldiv.find('.city-travletype'));
+        travletype.empty();
+        $.each(hotel.travletype, function(i,v){
+            travletype.append('<img src="styles/image/view/'+ v.toLowerCase()+'.png" title="'+v+'"/>');
+        });
+        $('#hotel-list ul').append(hoteldiv);
+    });
+}
